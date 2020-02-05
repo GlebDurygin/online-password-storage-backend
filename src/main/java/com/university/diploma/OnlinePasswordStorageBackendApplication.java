38ef0371b9c74ac7cf61ee392b2628beb4bef174
Gleb Durygin <durygin@haulmont.com>
2020-02-05 13:31:06 +0400

    Init commit

---
+++ src/main/java/com/university/diploma/OnlinePasswordStorageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.university.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePasswordStorageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePasswordStorageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/diploma/OnlinePasswordStorageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePasswordStorageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
