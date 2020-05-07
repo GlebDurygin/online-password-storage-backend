a2aaeaeb5ab9f65f2a091f7137fbd3f0026dd0ef
Gleb Durygin <durygin@haulmont.com>
2020-05-07 14:12:25 +0400

    Add testing module

---
+++ src/test/java/com/university/diploma/SelenideTest.java
@@ -0,0 +1,429 @@
+package com.university.diploma;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SelenideTest {
+
+    @Test
+    public void executeTest1() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test1");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest2() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test2");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest3() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test3");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest4() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test4");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest5() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test5");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest6() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test6");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest7() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test7");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest8() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test8");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest9() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test9");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest10() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test10");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest11() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test11");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest12() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test12");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest13() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test13");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest14() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test14");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest15() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test15");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest16() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test16");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest17() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test17");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest18() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test18");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest19() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test19");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest20() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test20");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest21() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test21");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest22() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test22");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest23() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test23");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest24() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test24");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest25() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test25");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest26() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test26");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest27() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test27");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest28() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test28");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest29() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test29");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest30() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test30");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest31() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test31");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest32() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test32");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest33() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test33");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest34() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test34");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest35() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test35");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest36() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test36");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest37() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test37");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest38() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test38");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest39() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test39");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest40() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test40");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest41() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test41");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest42() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test42");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest43() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test43");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest44() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test44");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest45() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test45");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest46() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test46");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest47() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test47");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest48() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test48");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest49() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test49");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    @Test
+    public void executeTest50() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        executeTest(driver, "test50");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
+    }
+
+    protected void executeTest(WebDriver driver, String str) {
+        driver.get("http://localhost:3000/sign-up");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[2]/form/div[1]/input"))
+                .sendKeys(str);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[2]/form/div[2]/input"))
+                .sendKeys(str);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[3]/button")).click();
+
+        driver.get("http://localhost:3000/sign-in");
+
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[2]/form/div[1]/input"))
+                .sendKeys(str);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[2]/form/div[2]/input"))
+                .sendKeys(str);
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/div/div[3]/div[3]/button"))
+                .click();
+    }
+}
